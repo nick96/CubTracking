cc8a7c121189a40b20999026a5d23c355906a1bb
Nick Spain <nicholas.spain96@gmail.com>
2019-07-01 14:21:11 +1000

    Update build.gradle

--- api/badgework/build.gradle.kts
+++ api/badgework/build.gradle.kts
@@ -1,18 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.31")
-    application
-}
-
 repositories {
+    mavenCentral()
     jcenter()
 }
 
-repositories {
-    mavenCentral()
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+plugins {
+    id("org.jetbrains.kotlin.jvm").version("1.3.31")
+    id("org.springframework.boot").version("2.1.6.RELEASE")
+//    id("jacoco")
+    application
+    java
+    checkstyle
 }
+
+apply(plugin = "io.spring.dependency-management")
 
 tasks {
     withType<KotlinCompile> {
@@ -20,16 +22,32 @@ tasks {
     }
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.31")
-    implementation("org.springframework.boot:spring-boot-starter-web:2.1.4.RELEASE")
 
-    testImplementation("de.codecentric.hikaku:hikaku-openapi:2.2.0")
-    testImplementation("de.codecentric.hikaku:hikaku-spring:2.2.0")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.1.4.RELEASE")
-    testImplementation("org.junit.platform:junit-platform-launcher:1.4.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.4.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.3.31")
+dependencies {
+    val springbootVer = "2.1.6.RELEASE"
+    val kotlinVer = "1.3.40"
+    val hikakuVer = "2.2.0"
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVer")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVer")
+
+    implementation("org.springframework.boot:spring-boot-starter-web:$springbootVer")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springbootVer")
+    implementation("org.springframework.boot:spring-boot-starter-security:$springbootVer")
+
+    implementation("com.auth0:auth0-spring-security-api:1.2.3")
+
+    runtime("com.h2database:h2:1.4.199")
+
+    testImplementation("de.codecentric.hikaku:hikaku-openapi:$hikakuVer")
+    testImplementation("de.codecentric.hikaku:hikaku-spring:$hikakuVer")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springbootVer")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
 }
 
 application {
