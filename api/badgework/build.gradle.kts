6f11a84c76b3fa839b6a7f347c5b51e67cd3ac88
Nick Spain <nicholas.spain96@gmail.com>
2019-07-20 14:46:38 +1000

    Move security dependency

--- api/badgework/build.gradle.kts
+++ api/badgework/build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web:$springbootVer")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springbootVer")
-    implementation("org.springframework.boot:spring-boot-starter-security:$springbootVer")
 
     runtime("com.h2database:h2:$h2Ver")
 
