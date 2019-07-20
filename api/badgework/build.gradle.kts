1ea54ba1d5d96a2e22d6559d8bbf33dbf3b3947c
Nick Spain <nicholas.spain96@gmail.com>
2019-07-20 14:38:36 +1000

    Cleanup gradle stuff

--- api/badgework/build.gradle.kts
+++ api/badgework/build.gradle.kts
@@ -1,32 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+plugins {
+    id("org.jetbrains.kotlin.jvm").version("1.3.41")
+    id("org.springframework.boot").version("2.1.6.RELEASE")
+    application
+    java
+}
+
 repositories {
     mavenCentral()
     jcenter()
 }
 
-plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.31")
-    id("org.springframework.boot").version("2.1.6.RELEASE")
-//    id("jacoco")
-    application
-    java
-    checkstyle
-}
-
-apply(plugin = "io.spring.dependency-management")
-
-tasks {
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-}
-
-
 dependencies {
     val springbootVer = "2.1.6.RELEASE"
-    val kotlinVer = "1.3.40"
-    val hikakuVer = "2.2.0"
+    val kotlinVer = "1.3.41"
+    val hikakuVer = "2.3.0"
+    val junitLauncherVer = "1.5.0"
+    val junitJupiterVer = "5.5.0"
+    val h2Ver = "1.4.199"
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVer")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVer")
@@ -35,25 +27,27 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springbootVer")
     implementation("org.springframework.boot:spring-boot-starter-security:$springbootVer")
 
-    implementation("com.auth0:auth0-spring-security-api:1.2.3")
-
-    runtime("com.h2database:h2:1.4.199")
+    runtime("com.h2database:h2:$h2Ver")
 
     testImplementation("de.codecentric.hikaku:hikaku-openapi:$hikakuVer")
     testImplementation("de.codecentric.hikaku:hikaku-spring:$hikakuVer")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:$springbootVer")
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.platform:junit-platform-launcher:$junitLauncherVer")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVer")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVer")
 }
 
 application {
     mainClassName = "me.nspain.cubtracking.badgework.BadgeworkApplicationKt"
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
 }
